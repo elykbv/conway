5a1550ace7c8f76f2b585ad0f9d767e680175487
elykbv <elii.yakubov@gmail.com>
2023-05-03 21:47:35 -0400

    Added Block component, grid, and click logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Block from "./components/Block/Block";
 
 function App() {
+    const [grid, setGrid] = useState<boolean[][]>(
+        Array(8)
+            .fill(false)
+            .map(() => Array(8).fill(false))
+    );
 
-  return (
-    <>
+    const handleClick = (row, col) => {
+        grid[row][col] = !grid[row][col];
+        setGrid([...grid]);
+    };
 
-    </>
-  )
+    return (
+        <>
+            {grid.map((row, rowIndex) => (
+                <div className="flex">
+                    {row.map((cell, colIndex) => (
+                        <Block clicked={cell} handleClick={handleClick} row={rowIndex} col={colIndex}/>
+
+                    ))}
+                </div>
+            ))}
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/Block/Block.tsx
@@ -0,0 +1,22 @@
+import { clsx } from "clsx";
+
+interface Props {
+    clicked: boolean
+    handleClick: () => void
+    row: number
+    col: number
+}
+
+const Block: React.FC<Props> = ({ clicked, handleClick, row, col }) => {
+    return (
+        <div
+            className={clsx(
+                "border-green-800 border-2 w-8 h-8",
+                clicked && "bg-gradient-to-br from-green-700 to-green-600"
+            )}
+            onClick={() => handleClick(row, col)}
+        ></div>
+    );
+};
+
+export default Block;
