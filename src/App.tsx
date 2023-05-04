970b5e975e3d8bdba1f46a8e2909f89f29e7add3
elykbv <elii.yakubov@gmail.com>
2023-05-04 08:55:52 -0400

    Basic generation logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Block from "./components/Block/Block";
 import Row from "./components/Row/Row";
 
 function App() {
+    const [gen, setGen] = useState<number>(0);
     const [grid, setGrid] = useState<boolean[][]>(
         Array(8)
             .fill(false)
@@ -14,10 +15,36 @@ function App() {
         grid[row][col] = !grid[row][col];
         setGrid([...grid]);
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, []);
+
+    const handleGenerationChange = (change: any): void => {
+        switch (change) {
+            case "increase":
+                setGen((prev) => prev + 1);
+                break;
+            case "decrease":
+                setGen((prev) => prev - 1);
+                break;
+        }
+    };
 
     return (
-        <div className="bg-gradient-to-r from-slate-900 to-slate-950 h-screen w-screen flex justify-center items-center">
+        <div className="bg-gradient-to-r from-slate-900 to-slate-950 h-screen w-screen flex flex-col justify-center items-center">
+            <div className="mb-10 text-green-500 flex gap-4 items-center">
+                <button
+                    className="bg-opacity-30 bg-white"
+                    onClick={() => handleGenerationChange("decrease")}
+                >
+                    -
+                </button>
+                {gen}
+                <button
+                    className="bg-opacity-30 bg-white"
+                    onClick={() => handleGenerationChange("increase")}
+                >
+                    +
+                </button>
+            </div>
             <div>
                 {grid.map((row, rowIndex) => (
                     <Row key={rowIndex}>
---
+++ src/types/GenerationChange.ts
@@ -0,0 +1,1 @@
+export type GenerationChange = "increase" | "decrease"
